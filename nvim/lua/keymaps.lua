4fea8d25632e1d24bc9a70c87ab9e27fb13d10a6
Ahmad Kabani <ahmadkabani@yahoo.com>
2024-08-30 22:43:09 -0400

    Update neovim code folding

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -81,5 +81,5 @@ keymap("n", "<C-n>", "<cmd>lua require('nvim-tree.api').tree.toggle()<cr>", opts
 keymap("n", "zR", "<cmd>lua require'ufo'.openAllFolds()<cr>", opts)
 keymap("n", "zM", "<cmd>lua require'ufo'.closeAllFolds()<cr>", opts)
 keymap("n", "zr", "<cmd>lua require'ufo'.openFoldsExceptKinds()<cr>", opts)
-keymap("n", "zm", "<cmd>lua require'ufo'.closeFoldsWith()<cr>", opts)
-keymap("n", "zp", "<cmd>lua require'ufo'.peekFoldedLinesUnderCursor()<cr>", opts)
+--keymap("n", "zm", "<cmd>lua require'ufo'.closeFoldsWith(1)<cr>", opts)
+--keymap("n", "zp", "<cmd>lua require'ufo'.peekFoldedLinesUnderCursor()<cr>", opts)
