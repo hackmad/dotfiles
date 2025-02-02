12abeeb56a8eccc0cf8d72a4ec5202a1f7e75d4a
Ahmad Kabani <ahmadkabani@yahoo.com>
2025-02-02 14:02:35 -0500

    Revert use of tab/s-tab for completion as it interferes with normal editing

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -137,28 +137,28 @@ return {
                     ["<C-d>"] = cmp.mapping.scroll_docs(4),
 
                     -- Use Tab and S-Tab for next/previous entry.
-                    ["<Tab>"] = cmp.mapping(function (fallback)
-                        if luasnip.expandable() then
-                            luasnip.expand()
-                        elseif cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.jumpable(1) then
-                            luasnip.jump(1)
-                        elseif vim.api.nvim_get_mode().mode == 'i' then
-                            tabout.tabout()
-                        else
-                            fallback()
-                        end
-                    end, {'i', 's'}),
-                    ["<S-Tab>"] = cmp.mapping(function (fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, {'i', 's'}),
+                    --["<Tab>"] = cmp.mapping(function (fallback)
+                    --    if luasnip.expandable() then
+                    --        luasnip.expand()
+                    --    elseif cmp.visible() then
+                    --        cmp.select_next_item()
+                    --    elseif luasnip.jumpable(1) then
+                    --        luasnip.jump(1)
+                    --    elseif vim.api.nvim_get_mode().mode == 'i' then
+                    --        tabout.tabout()
+                    --    else
+                    --        fallback()
+                    --    end
+                    --end, {'i', 's'}),
+                    --["<S-Tab>"] = cmp.mapping(function (fallback)
+                    --    if cmp.visible() then
+                    --        cmp.select_prev_item()
+                    --    elseif luasnip.jumpable(-1) then
+                    --        luasnip.jump(-1)
+                    --    else
+                    --        fallback()
+                    --    end
+                    --end, {'i', 's'}),
                 }),
                 snippet = {
                     expand = function(args)
